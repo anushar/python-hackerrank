2009574e0db54fe85f86aa6492c87b5196319ff5
anushar <anushar@users.noreply.github.com>
2016-11-16 16:51:16 +0000

    Create find_maximum_function.py

---
+++ find_maximum_function.py
@@ -0,0 +1,24 @@
+#Find the maximized value  Smax obtained.%  denotes the modulo operator. 
+#Input Format  
+#The first line contains  space separated integers  and . 
+#The next  lines each contains an integer  followed by  space separated integers denoting the elements in the list. 
+Output Format  Output a single integer denoting the value . 
+
+import itertools
+(K, N) = map(int, raw_input().split()) 
+L = list() 
+for i in range(K):     
+  l = map(int, raw_input().split())    
+  n = l[0]     
+  L.append(l[1:])     
+  assert len(L[i]) == n 
+  
+ S_max = 0 
+ L_max = None 
+ 
+for l in itertools.product(*L):     
+  s = sum([x**2 for x in l]) % N     
+  if s > S_max:         
+    S_max = s         
+    L_max = l         
+print S_max 
