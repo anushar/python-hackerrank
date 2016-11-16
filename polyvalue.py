ea52f3b105833dc7d482efdd538974bff7378942
anushar <anushar@users.noreply.github.com>
2016-11-16 11:30:28 +0000

    Update polyvalue.py
    read coeff's of polygon from stdin and find value of polygon at x

--- polyvalue.py
+++ polyvalue.py
@@ -1,1 +1,7 @@
+
+# read coeff's of polygon from stdin and find value of polygon at x
 import numpy
+
+coeff = numpy.array(raw_input().split(),float)
+x = int(raw_input())
+print numpy.polyval(coeff,x)
