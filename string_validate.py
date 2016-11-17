241310f23bf08ac2c4210e3376086a02c423f95b
anushar <anushar@users.noreply.github.com>
2016-11-17 10:10:25 +0000

    Create string_validate.py

---
+++ string_validate.py
@@ -0,0 +1,7 @@
+# check if anycharecters of input string are alpha,digit etc
+s = raw_input()
+print any(i.isalnum() for i in s)
+print any(i.isalpha() for i in s)
+print any(i.isdigit() for i in s)
+print any(i.islower() for i in s)
+print any(i.isupper() for i in s)
